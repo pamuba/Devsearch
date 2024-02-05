6b6012ff40779dc42f4a6a70bab296bf0a8f0134
pamuba <me.in@live.in>
2024-02-05 23:29:57 +0530

    5feb-2

--- projects/views.py
+++ projects/views.py
@@ -1,13 +1,36 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+pojectsList = [
+    {
+        "id": "1",
+        "title": "Haruki Murakami",
+        "description":"FIRST PROJECT Completed"
+    },
+    {
+        "id":"2",
+        "title": "Oscar Wilde",
+        "description": "The Picture of Dorian Gray",
+    }
+]
 
 def projects(request):
-    return render(request, 'projects.html')
+    msg = " you are on the project page"
+    number  = 1
+    context = {'msg':msg, 'number':number, 'projects':pojectsList}
+    # combine main+projects html files -> insert the value of msg
+    # return the result inside HttpResponse ob to the browser   
+
+    return render(request, 'projects/projects.html', context)
     # return HttpResponse("Here are our products")
 
 
 def project(request, pk):
-    return render(request, 'single-project.html')
+    projectObj = None
+    for project in pojectsList:
+        if str(project["id"]) == pk:
+            projectObj = project
+
+    return render(request, 'projects/single-project.html', {'project':projectObj})
     # return HttpResponse("Here is our products "+pk)
 
