ee9281d58da56283b55a04f57b9de97279898064
pamuba <me.in@live.in>
2024-02-06 23:27:10 +0530

    6th Feb

--- projects/urls.py
+++ projects/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.projects, name='projects'),
-    path('project/<str:pk>/', views.project, name='project'),
+    path(':project/<str:pk>/', views.project, name='project'),
 ]
  
--- projects/views.py
+++ projects/views.py
@@ -1,23 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Project, Review, Tag
 
-pojectsList = [
-    {
-        "id": "1",
-        "title": "Haruki Murakami",
-        "description":"FIRST PROJECT Completed"
-    },
-    {
-        "id":"2",
-        "title": "Oscar Wilde",
-        "description": "The Picture of Dorian Gray",
-    }
-]
+# pojectsList = [
+#     {
+#         "id": "1",
+#         "title": "Haruki Murakami",
+#         "description":"FIRST PROJECT Completed"
+#     },
+#     {
+#         "id":"2",
+#         "title": "Oscar Wilde",
+#         "description": "The Picture of Dorian Gray",
+#     }
+# ]
 
 def projects(request):
-    msg = " you are on the project page"
-    number  = 1
-    context = {'msg':msg, 'number':number, 'projects':pojectsList}
+    projects = Project.objects.all()
+    context = {'projects':projects}
+    # msg = " you are on the project page"
+    # number  = 1
+    # context = {'msg':msg, 'number':number, 'projects':pojectsList}
     # combine main+projects html files -> insert the value of msg
     # return the result inside HttpResponse ob to the browser   
 
@@ -26,11 +29,12 @@ def projects(request):
 
 
 def project(request, pk):
-    projectObj = None
-    for project in pojectsList:
-        if str(project["id"]) == pk:
-            projectObj = project
+    projectObj = Project.objects.get(id=pk)
+    tags = projectObj.tags.all()
+    # for project in pojectsList:
+    #     if str(project["id"]) == pk:
+    #         projectObj = project
 
-    return render(request, 'projects/single-project.html', {'project':projectObj})
+    return render(request, 'projects/single-project.html', {'project':projectObj, 'tags':tags})
     # return HttpResponse("Here is our products "+pk)
 
